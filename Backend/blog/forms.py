d721bc69e606698e3ee4c16c81bcff46eca44bdc
Mubin <kmhmubin@gmail.com>
2021-09-04 21:56:01 +0600

    feat #5: add create post from using Article form

---
+++ Backend/blog/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+
+from blog.models import Article
+
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'excerpt', 'content', 'tags']
