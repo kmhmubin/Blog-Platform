4a199d69d0c3d845ce9e7c0af683c52f8d7f2b21
Mubin <kmhmubin@gmail.com>
2021-09-05 01:55:39 +0600

    fix #5: user model fields input

--- Backend/users/admin.py
+++ Backend/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+# Display custom list for Article model
+from .models import Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("name", "username", "email", "created")
+
+
+admin.site.register(Profile, UserAdmin)
--- Backend/users/models.py
+++ Backend/users/models.py
@@ -7,14 +7,22 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=200, blank=True, null=True)
+    name = models.CharField('Full Name', max_length=200, blank=True, null=True)
     username = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=200, null=True, blank=True)
     bio = models.TextField(max_length=200, blank=True, null=True)
     profile_image = models.ImageField(upload_to='profiles', blank=True, null=True, default='astronaut.svg')
-    location = models.CharField(max_length=200, null=True, blank=True)
-    address = models.OneToOneField('BillingAddress', blank=True, null=True, on_delete=models.SET_NULL)
-    social_links = models.ManyToManyField('SocialLinks', blank=True)
+    address_1 = models.CharField('Present Address', max_length=128, null=True, blank=True)
+    address_2 = models.CharField('Present Address cont..', max_length=128, blank=True, null=True)
+    city = models.CharField('City: Dhaka', max_length=65, blank=True, null=True)
+    country = models.CharField('Country: Bangladesh', max_length=70, blank=True, null=True)
+    zip = models.CharField('Zip Code: 1234', max_length=4, blank=True, null=True)
+    github = models.URLField('GitHub URL Link', null=True, blank=True)
+    twitter = models.URLField('Twitter URL Link', null=True, blank=True)
+    linkedin = models.URLField('Linkedin URL Link', null=True, blank=True)
+    facebook = models.URLField('Facebook URL Link', null=True, blank=True)
+    youtube = models.URLField('YouTube URL Link', null=True, blank=True)
+    website = models.URLField('Personal Website Link', null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
@@ -24,36 +32,3 @@ class Profile(models.Model):
 
     class Meta:
         ordering = ['name']
-
-
-# Billing Address Model
-class BillingAddress(models.Model):
-    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, null=True)
-    address_1 = models.CharField('Present Address', max_length=128, null=True, blank=True)
-    address_2 = models.CharField('Present Address cont..', max_length=128, blank=True, null=True)
-    city = models.CharField('City: Dhaka', max_length=65, blank=True, null=True)
-    country = models.CharField('Country: Bangladesh', max_length=70, blank=True, null=True)
-    zip = models.CharField('Zip Code: 1234', max_length=4, blank=True, null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-
-    def __str__(self):
-        return self.address_1, self.address_2, self.city, self.country, self.zip
-
-
-# Social Link Model
-class SocialLinks(models.Model):
-    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, null=True)
-    github = models.URLField(null=True, blank=True)
-    twitter = models.URLField(null=True, blank=True)
-    linkedin = models.URLField(null=True, blank=True)
-    facebook = models.URLField(null=True, blank=True)
-    youtube = models.URLField(null=True, blank=True)
-    website = models.URLField(null=True, blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-
-    def __str__(self):
-        return self.github, self.twitter, self.linkedin, self.facebook, self.youtube, self.website
