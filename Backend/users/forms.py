7aef000971c9a3da92b167a05f2ad10383d0f085
Mubin <kmhmubin@gmail.com>
2021-09-08 15:11:32 +0600

    refactor #5: fully working form with custom design

--- Backend/users/forms.py
+++ Backend/users/forms.py
@@ -3,19 +3,6 @@ from django.contrib.auth.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
-    # username = forms.CharField(
-    #     min_length=4,
-    #     max_length=150,
-    #     widget=forms.TextInput(attrs={'class': 'form-control'})
-    # )
-    # email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    # password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}),
-    #                             help_text=password_validation.password_validators_help_text_html())
-    # password2 = forms.CharField(
-    #     widget=forms.PasswordInput(attrs={'class': 'form-control'}),
-    #     help_text='Enter the same password'
-    # )
-
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
