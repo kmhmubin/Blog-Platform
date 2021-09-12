0d790ec88bc0f2a1cc7cc14a83d1301283d8c34e
K M H Mubin <kmhmubin@gmail.com>
2021-09-13 01:25:03 +0600

    feat #5: add user model
    add user model and migrate to DB, and add the model into Admin Panel

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,31 @@
+import uuid
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+# Author Profile Model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=150, blank=True, null=True)
+    email = models.EmailField(max_length=200, blank=True, null=True)
+    profile_tagline = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profiles', blank=True, null=True, default='astronaut.jpg')
+    address_1 = models.CharField('Present Address', max_length=128, null=True, blank=True)
+    address_2 = models.CharField('Present Address cont..', max_length=128, blank=True, null=True)
+    city = models.CharField('City', max_length=65, blank=True, null=True)
+    zip = models.CharField('Zip Code', max_length=4, blank=True, null=True)
+    country = models.CharField('Country', max_length=70, blank=True, null=True)
+    github = models.URLField('GitHub URL Link', null=True, blank=True)
+    twitter = models.URLField('Twitter URL Link', null=True, blank=True)
+    linkedin = models.URLField('Linkedin URL Link', null=True, blank=True)
+    facebook = models.URLField('Facebook URL Link', null=True, blank=True)
+    youtube = models.URLField('YouTube URL Link', null=True, blank=True)
+    website = models.URLField('Personal Website Link', null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.user.username)
