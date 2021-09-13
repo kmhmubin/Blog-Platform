f299ecc4184e686651e171f96f7189bb52bd325e
K M H Mubin <kmhmubin@gmail.com>
2021-09-13 21:54:43 +0600

    remove #5: due to windows crushed

--- backend/user/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from user.models import Profile
-
-
-@admin.register(Profile)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("name", "email", "created")
