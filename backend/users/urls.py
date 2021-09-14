adf5877ac09905afc7fe71ab01dba7f9e642f562
K M H Mubin <kmhmubin@gmail.com>
2021-09-14 21:18:58 +0600

    feat #5: add all authors view
    add a temporary all author profile page

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.authors, name='profiles'),
+    path('author/<str:pk>', views.author_profile, name='author-profile'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from users.models import Profile
 
-# Create your views here.
+
+# All author profile view
+def authors(request):
+    # fetch all the author profile from db
+    authors_profile = Profile.objects.all()
+    context = {'profiles': authors_profile}
+    return render(request, 'users/authors.html', context)
+
+
+# Get a specific author profile
+def author_profile(request, pk):
+    # get the specific user
+    author = Profile.objects.get(id=pk)
+    context = {'profile': author}
+    return render(request, 'users/author-profile.html', context)
