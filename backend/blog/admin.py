e17d2db5df2e2cecbc37fb55889ff6efa7284262
K M H Mubin <kmhmubin@gmail.com>
2021-09-18 21:11:30 +0600

    feat #5: add article model and register the model in Admin panel

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog.models import Article
 
-# Register your models here.
+
+# Register the article model
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,31 @@
+from autoslug import AutoSlugField
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from users.models import Profile
+
+
+# article model
+class Article(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('publish', 'Publish'),
+    )
+    author = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL, related_name='blog_posts')
+    title = models.CharField(max_length=200)
+    slug = AutoSlugField(populate_from='title', unique=True, null=False, db_index=True, unique_for_date='publish')
+    excerpt = models.CharField(max_length=60)
+    featured_image = models.ImageField(upload_to="posts", null=True, blank=True, default="default.jpg")
+    content = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    # sort out by publish date
+    class Meta:
+        ordering = ('-publish',)
+
+    # display the article name
+    def __str__(self):
+        return self.title
